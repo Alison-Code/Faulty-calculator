2ea2ead2a2c5dd7ec4200162f4f05dfca422e12a
Victorr-Codes <72153622+Victorr-Codes@users.noreply.github.com>
2020-10-02 14:09:37 +0530

    Update exercise 2 FAULTY CALCULATOR.py

--- exercise 2 FAULTY CALCULATOR.py
+++ exercise 2 FAULTY CALCULATOR.py
@@ -1,18 +1,32 @@
-print("WELCOM TO VICTOR's CALCULATOR")
-print("NTER YOUR FIRST NUMBER")
-var1 = it(input())
-print("WATDO YOU WANT TO DO")
-pri("+" ,"" ,"/" , "*")
-var3 = iput()
-print("ENTER YO
+print("WELCOME TO VICTOR's CALCULATOR")
+print("ENTER YOUR FIRST NUMBER")
+var1 = int(input())
+print("WHAT DO YOU WANT TO DO")
+print("+" ,"-" ,"/" , "*")
+var3 = input()
+print("ENTER YOUR SECOND NUMBER")
+var2 = int(input())
+
 if var3=="+":
-    if var1==56 d var2==9:
+    if var1==56 and var2==9:
         print("Your ans is 77")
     else:
-        print("Yur ans is" , var1 + var2)
+        print("Your ans is" , var1 + var2)
         
-       
+        
 elif var3=="*":
-    if v
+    if var1==45 and var2==3:
+        print("Your ans is 555")
+    else:
+        print("Your ans is" , var1 * var2)
 
-elif
+
+elif var3=="/":
+    if var1==56 and var2==6:
+        print("Your ans is 4")
+    else:
+        print("Your ans is" , var1 / var2)
+
+
+else:
+    print("Your ans is" , var1-var2)
